9d52db682ef1d5e3e96769b9a1727abb0836ac16
JyothsnaKotyada <82897244+jyothsna1899@users.noreply.github.com>
2022-06-03 14:24:42 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 class hello{  
     public static void main(String args[]){  
      System.out.println("Hello from Java");  
+     System.out.println("This is a sample java program");  
     }  
 }  
